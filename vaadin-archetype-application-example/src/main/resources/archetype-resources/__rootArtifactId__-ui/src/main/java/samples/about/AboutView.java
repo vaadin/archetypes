4e8f0445bac42ad85a77be931df2804779242ad5
Pekka Hyvönen <pekka@vaadin.com>
2017-02-02 12:52:56 +0200

    Fix DataProvider changes and ContentMode moving

--- vaadin-archetype-application-example/src/main/resources/archetype-resources/__rootArtifactId__-ui/src/main/java/samples/about/AboutView.java
+++ vaadin-archetype-application-example/src/main/resources/archetype-resources/__rootArtifactId__-ui/src/main/java/samples/about/AboutView.java
@@ -7,7 +7,7 @@ import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.server.FontAwesome;
 import com.vaadin.shared.Version;
-import com.vaadin.shared.ui.label.ContentMode;
+import com.vaadin.shared.ui.ContentMode;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.CustomLayout;
 import com.vaadin.ui.Label;
