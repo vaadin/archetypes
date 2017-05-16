153603bc58b21b283f32be9399351c3ebf1b886d
Henri Sara <henri.sara@gmail.com>
2017-05-16 16:24:29 +0300

    Update class names for OSGi resource related classes (#130)

--- vaadin-archetype-liferay-portlet/src/main/resources/archetype-resources/src/main/java/AppTheme.java
+++ vaadin-archetype-liferay-portlet/src/main/resources/archetype-resources/src/main/java/AppTheme.java
@@ -2,11 +2,11 @@ package ${package};
 
 import org.osgi.service.component.annotations.Component;
 
-import com.vaadin.osgi.resources.OSGiVaadinTheme;
+import com.vaadin.osgi.resources.OsgiVaadinTheme;
 import com.vaadin.ui.themes.ValoTheme;
 
-@Component(immediate = true, service = OSGiVaadinTheme.class)
-public class AppTheme extends ValoTheme implements OSGiVaadinTheme {
+@Component(immediate = true, service = OsgiVaadinTheme.class)
+public class AppTheme extends ValoTheme implements OsgiVaadinTheme {
     @Override
     public String getName() {
         return "${theme}";
--- vaadin-archetype-liferay-portlet/src/main/resources/archetype-resources/src/main/java/AppWidgetSet.java
+++ vaadin-archetype-liferay-portlet/src/main/resources/archetype-resources/src/main/java/AppWidgetSet.java
@@ -2,10 +2,10 @@ package ${package};
 
 import org.osgi.service.component.annotations.Component;
 
-import com.vaadin.osgi.resources.OSGiVaadinWidgetset;
+import com.vaadin.osgi.resources.OsgiVaadinWidgetset;
 
-@Component(immediate = true, service = OSGiVaadinWidgetset.class)
-public class AppWidgetSet implements OSGiVaadinWidgetset {
+@Component(immediate = true, service = OsgiVaadinWidgetset.class)
+public class AppWidgetSet implements OsgiVaadinWidgetset {
 
     @Override
     public String getName() {
