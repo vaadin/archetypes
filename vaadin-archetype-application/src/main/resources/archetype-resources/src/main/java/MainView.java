996b527c073fc2dfae8ca7f09615edad8b910d8e
Matti Tahvonen <matti@vaadin.com>
2021-03-09 11:44:40 +0200

    chore: Vaadin 19 compatibility (#175)
    * chore: Vaadin 19 compatibility
    
     * updated versions of both vaadin and other dependencies
     * removed obsoleted exclusions and added new ones
     * Created AppShell for the PWA annotations
    
    * removed a non-best practice configuration
    
    * removed obsolete files
    
    These files are generated automatically by the build, so it doesn't make sense
    keeping them in the archetype and to maintain those manually
    
    * Build by default against a version available in central

--- vaadin-archetype-application/src/main/resources/archetype-resources/src/main/java/MainView.java
+++ vaadin-archetype-application/src/main/resources/archetype-resources/src/main/java/MainView.java
@@ -6,14 +6,12 @@ package ${package};
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.router.Route;
 
 /**
  * The main view contains a button and a click listener.
  */
 @Route
-@PWA(name = "${applicationName}", shortName = "${applicationName}")
 public class MainView extends VerticalLayout {
 
     public MainView() {
