8b29f885a5e80177fb9d0cddf37b81599868779a
Tarek Oraby <42799254+tarekoraby@users.noreply.github.com>
2021-09-08 10:36:44 +0300

    Remove addon

--- vaadin-archetype-spring-application/src/main/resources/archetype-resources/src/main/java/Application.java
+++ vaadin-archetype-spring-application/src/main/resources/archetype-resources/src/main/java/Application.java
@@ -6,7 +6,6 @@ package ${package};
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.vaadin.artur.helpers.LaunchUtil;
 
 /**
  * The entry point of the Spring Boot application.
@@ -15,7 +14,7 @@ import org.vaadin.artur.helpers.LaunchUtil;
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        LaunchUtil.launchBrowserInDevelopmentMode(SpringApplication.run(Application.class, args));
+        SpringApplication.run(Application.class, args);
     }
 
 }
