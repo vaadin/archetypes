f97741b99d1c9dbb596cce3dc0f48a49bfed1e03
Matti Tahvonen <matti@vaadin.com>
2022-09-01 13:19:49 +0300

    Removed obsolete interfaces (#199)
    Only needed for war projects.
    
    Same change as here: https://github.com/vaadin/skeleton-starter-flow-spring/pull/849

--- vaadin-archetype-spring-application/src/main/resources/archetype-resources/src/main/java/Application.java
+++ vaadin-archetype-spring-application/src/main/resources/archetype-resources/src/main/java/Application.java
@@ -5,13 +5,12 @@ package ${package};
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * The entry point of the Spring Boot application.
  */
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
