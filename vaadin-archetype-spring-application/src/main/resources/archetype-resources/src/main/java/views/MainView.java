d18e6ff727a0e9288549ccf47a2b6c091a1a59e2
Matti Tahvonen <matti@vaadin.com>
2023-03-08 09:04:43 +0200

    Update MainView.java (#202)
    Removed odd and irrelevant comment
    
    Co-authored-by: Zhe Sun <31067185+ZheSun88@users.noreply.github.com>

--- vaadin-archetype-spring-application/src/main/resources/archetype-resources/src/main/java/views/MainView.java
+++ vaadin-archetype-spring-application/src/main/resources/archetype-resources/src/main/java/views/MainView.java
@@ -8,9 +8,6 @@ import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
-/**
- * The main view is a top-level placeholder for other views.
- */
 @Route
 public class MainView extends VerticalLayout {
 
